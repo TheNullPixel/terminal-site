40338967321b6ec3849a64a560ac73127a3a4f2a
null-pixel-dev <nullpixeldev@proton.me>
2024-03-16 23:18:50 +0000

    refactored js

--- terminal.js
+++ terminal.js
@@ -1,52 +1,110 @@
+// Element references
 var before = document.getElementById("before");
 var liner = document.getElementById("liner");
 var command = document.getElementById("typer");
 var textarea = document.getElementById("texter");
 var terminal = document.getElementById("terminal");
+var cursor;
 
+// Data variables
 var git = 0;
 var commands = [];
+var twitter = "https://www.twitter.com/nullpixeldev/";
+var github = "https://github.com/null-pixel-dev/";
+var email = "mailto:nullpixeldev@proton.me";
 
-setTimeout(function () {
-  loopLines(banner, "", 80);
-  textarea.focus();
-}, 100);
+// Static content
+var about = [
+  "<br>",
+  "Hey there, I'm Null Pixel!👋",
+  "I'm a frontend developer from Belfast, UK. I'm a final year",
+  "Software Engineering student at Queen's University Belfast,",
+  "also working part time as a placement engineer at DailyPay.",
+  "<br>",
+];
 
-window.addEventListener("keyup", enterKey);
+var social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/nullpixeldev</a>',
+  'github         <a href="' + github + '" target="_blank">github/null-pixel-dev</a>',
+  "<br>",
+];
 
-textarea.value = "";
-command.innerHTML = textarea.value;
+var projects = [
+  "<br>",
+  "Coming soon!",
+  "<br>",
+];
 
+var help = [
+  "<br>",
+  '<span class="command">about</span>          Who is Null Pixel?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View project portfolio',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           Display a list of available commands',
+  '<span class="command">email</span>          Get in contact with me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+var banner = [
+  '<span class="index">Null Pixel (null-pixel-dev). All pixels nullified.</span>',
+  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="accent">Welcome to my terminal website.</span>',
+  '<span class="accent">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="accent">.</span>',
+];
+
+// Initialization
+window.onload = function init() {
+  cursor = document.getElementById("cursor");
+  cursor.style.left = "0px";
+
+  setTimeout(function () {
+    loopLines(banner, "", 80);
+    textarea.focus();
+  }, 100);
+
+  textarea.value = "";
+  command.innerHTML = textarea.value;
+  window.addEventListener("keyup", enterKey);
+};
+
+// Functions
 function enterKey(e) {
   if (e.keyCode == 181) {
     document.location.reload(true);
   }
   if (e.keyCode == 13) {
-    commands.push(command.innerHTML);
-    git = commands.length;
-    addLine(
-      "user@null-pixel-dev:~$ " + command.innerHTML,
-      "no-animation",
-      0
-    );
-    commander(command.innerHTML.toLowerCase());
-    command.innerHTML = "";
-    textarea.value = "";
+    // Enter key logic
+    executeCommand();
   }
   if (e.keyCode == 38 && git != 0) {
-    git -= 1;
-    textarea.value = commands[git];
-    command.innerHTML = textarea.value;
+    // Up arrow key logic
+    navigateCommands(-1);
   }
   if (e.keyCode == 40 && git != commands.length) {
-    git += 1;
-    if (commands[git] === undefined) {
-      textarea.value = "";
-    } else {
-      textarea.value = commands[git];
-    }
-    command.innerHTML = textarea.value;
+    // Down arrow key logic
+    navigateCommands(1);
   }
+}
+
+function executeCommand() {
+  commands.push(command.innerHTML);
+  git = commands.length;
+  addLine("user@null-pixel-dev:~$ " + command.innerHTML, "no-animation", 0);
+  commander(command.innerHTML.toLowerCase());
+  command.innerHTML = "";
+  textarea.value = "";
+}
+
+function navigateCommands(direction) {
+  git += direction;
+  if (direction === -1 || (direction === 1 && commands[git] !== undefined)) {
+    textarea.value = commands[git] ?? "";
+  }
+  command.innerHTML = textarea.value;
 }
 
 function commander(cmd) {
@@ -104,12 +162,6 @@ function commander(cmd) {
   }
 }
 
-function newTab(link) {
-  setTimeout(function () {
-    window.open(link, "_blank");
-  }, 500);
-}
-
 function addLine(text, style, time) {
   var t = "";
   for (let i = 0; i < text.length; i++) {
@@ -136,86 +188,9 @@ function loopLines(name, style, time) {
     addLine(item, style, index * time);
   });
 }
-var twitter = "https://www.twitter.com/nullpixeldev/";
-var github = "https://github.com/null-pixel-dev/";
-var email = "mailto:nullpixeldev@proton.me";
 
-about = [
-  "<br>",
-  "Hey there, I'm Null Pixel!👋",
-  "I'm a frontend devloper from Belfast, UK. I'm a final year",
-  "Software Engineering student at Queen's University Belfast,",
-  "also working part time as a placement engineer at DailyPay.",
-  "<br>",
-];
-
-social = [
-  "<br>",
-  'twitter        <a href="' +
-    twitter +
-    '" target="_blank">twitter/nullpixeldev' +
-    "</a>",
-  'github         <a href="' +
-    github +
-    '" target="_blank">github/null-pixel-dev' +
-    "</a>",
-  "<br>",
-];
-
-projects = ["<br>", "Coming soon!", "<br>"];
-
-help = [
-  "<br>",
-  '<span class="command">about</span>          Who is Null Pixel?',
-  '<span class="command">social</span>         Display social networks',
-  '<span class="command">projects</span>       View project portfolio',
-  '<span class="command">history</span>        View command history',
-  '<span class="command">help</span>           Display a list of available commands',
-  '<span class="command">email</span>          Get in contact with me',
-  '<span class="command">clear</span>          Clear terminal',
-  '<span class="command">banner</span>         Display the header',
-  "<br>",
-];
-
-banner = [
-  '<span class="index">Null Pixel (null-pixel-dev). All pixels nullified.</span>',
-  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
-  '<span class="accent">Welcome to my terminal website.</span>',
-  '<span class="accent">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="accent">.</span>',
-];
-function $(elid) {
-  return document.getElementById(elid);
-}
-
-var cursor;
-window.onload = init;
-
-function init() {
-  cursor = $("cursor");
-  cursor.style.left = "0px";
-}
-
-function nl2br(txt) {
-  return txt.replace(/\n/g, "");
-}
-
-function typeIt(from, e) {
-  e = e || window;
-  var w = $("typer");
-  var tw = from.value;
-  w.innerHTML = nl2br(tw);
-}
-
-function moveIt(count, e) {
-  e = e || window;
-  var keycode = e.keyCode || e.which;
-  if (keycode == 37 && parseInt(cursor.style.left) >= 0 - (count - 1) * 10) {
-    cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
-  } else if (keycode == 39 && parseInt(cursor.style.left) + 10 <= 0) {
-    cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
-  }
-}
-
-function alert(txt) {
-  console.log(txt);
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, "_blank");
+  }, 500);
 }
